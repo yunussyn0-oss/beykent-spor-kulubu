4fa8825642f78b39b43e087e6902f4813dbc7b76
yunussyn0-oss <yunussyn0@icloud.com>
2026-03-13 17:24:38 +0300

    En basit test

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.UseStaticFiles();
-app.UseRouting();
-app.MapControllerRoute(name: "default", pattern: "{controller=Antrenor}/{action=Giris}/{id?}");
+app.MapGet("/", () => "Selam! Uygulama çalışıyor.");
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
